5d521a218a7aff2028c3e7e35f7981f0ea1eeef2
Cassandra de Git <cassandra.m.degit@gmail.com>
2014-12-08 05:10:56 +0000

    Switch to slightly less awful colors

--- app/assets/javascripts/siattips/views/_category_list_item_view.js
+++ app/assets/javascripts/siattips/views/_category_list_item_view.js
@@ -1,6 +1,9 @@
 App.Views.CategoryListItemView = Marionette.ItemView.extend({
   template: JST['siattips/templates/views/category_list_item_template'],
   tagName: 'li',
+  className: function() {
+  	return this.model.get("name").toLowerCase();
+  },
   attributes: function() {
     return {
       "data-filter": this.model.get("name")
